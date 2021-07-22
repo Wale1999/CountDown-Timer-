85c09c51a62bdd1ed632d16c6d66b9980fe684e7
Ayoola Abraham <abrahamayoola4@gmail.com>
2021-07-22 11:19:54 +0100

    Uploading the final works before i push them to my github

--- JS/countdown.js
+++ JS/countdown.js
@@ -1,4 +1,25 @@
-let countDown = function() {
-    const date = new Date('June 26, 2022 00:00:00').getTime;
+let countDown = () => {
+    const date = new Date('June 26, 2022 00:00:00').getTime();
+    const currentTime = new Date().getTime();
+    const subtractTime = date - currentTime;
 
+    // How does these works? Lets go!
+    const seconds = 1000;
+    const minutes = seconds * 60;
+    const hour = minutes * 60;
+    const day = hour * 24;
+
+    //Let's calculate them 
+
+    const mainDay = Math.floor(subtractTime / day);
+    const mainHour = Math.floor((subtractTime % day) / hour);
+    const mainMinutes = Math.floor((subtractTime % hour) / minutes);
+    const mainSeconds = Math.floor((subtractTime % minutes) / seconds);
+
+    //functionalities and the mechanics that makes our timer work
+    document.querySelector('.day').innerText = mainDay;
+    document.querySelector('.hour').innerText = mainHour;
+    document.querySelector('.minutes').innerText = mainMinutes;
+    document.querySelector('.seconds').innerText = mainSeconds;
 }
+setInterval(countDown, 1000)
